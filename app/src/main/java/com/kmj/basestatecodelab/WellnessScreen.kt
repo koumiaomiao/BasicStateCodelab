0913045bb62bc96bbd9d602c52cdb5c20f9f5ef2
mjkou <mjkou@thoughtworks.com>
2024-10-24 14:02:06 +0800

    add event

---
+++ app/src/main/java/com/kmj/basestatecodelab/WellnessScreen.kt
@@ -0,0 +1,27 @@
+package com.kmj.basestatecodelab
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kmj.basestatecodelab.ui.theme.BaseStateCodelabTheme
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+@Preview
+@Composable
+fun WellnessScreenPreview() {
+    BaseStateCodelabTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            WellnessScreen()
+        }
+    }
+}
