d67d12096286cb1d7f11e8b7ebd7cbf1172abced
mjkou <mjkou@thoughtworks.com>
2024-10-24 14:15:12 +0800

    refactor - use delegate

--- app/src/main/java/com/kmj/basestatecodelab/WaterCount.kt
+++ app/src/main/java/com/kmj/basestatecodelab/WaterCount.kt
@@ -5,9 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,11 +18,11 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = Modifier.padding(16.dp)) {
 
         // Changes to count are now tracked by Compose
-        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember { mutableStateOf(0) }
 
-        Text("You've had ${count.value} glasses.")
+        Text("You've had $count glasses.")
         Button(
-            onClick = { count.value++ },
+            onClick = { count++ },
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text("Add One")
