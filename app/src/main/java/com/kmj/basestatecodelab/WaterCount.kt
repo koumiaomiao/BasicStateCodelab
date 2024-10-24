1d60fccfd61154e7586fd72e3e4f54845b3fba4a
mjkou <mjkou@thoughtworks.com>
2024-10-24 14:13:37 +0800

    use remember to save composition value

--- app/src/main/java/com/kmj/basestatecodelab/WaterCount.kt
+++ app/src/main/java/com/kmj/basestatecodelab/WaterCount.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -12,17 +15,16 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = Modifier.padding(16.dp)) {
-        var count = 0
-        Text(
-            text = "You've had $count glasses."
-        )
+
+        // Changes to count are now tracked by Compose
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
+
+        Text("You've had ${count.value} glasses.")
         Button(
-            onClick = { count++ },
+            onClick = { count.value++ },
             modifier = Modifier.padding(top = 8.dp)
         ) {
-            Text(
-                text = "Add One"
-            )
+            Text("Add One")
         }
     }
 }
