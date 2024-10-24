6c161c703c41f46f9af05000d17997c586940ce9
mjkou <mjkou@thoughtworks.com>
2024-10-24 15:16:47 +0800

    remember func in composition

--- app/src/main/java/com/kmj/basestatecodelab/WaterCount.kt
+++ app/src/main/java/com/kmj/basestatecodelab/WaterCount.kt
@@ -1,6 +1,7 @@
 package com.kmj.basestatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -17,20 +18,26 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = Modifier.padding(16.dp)) {
 
-        // Changes to count are now tracked by Compose
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            // This text is present if the button has been clicked
-            // at least once; absent otherwise
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
             Text("You've had $count glasses.")
         }
-        Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
-            Text("Add One")
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
