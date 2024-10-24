ec4c79b73ffc6b16e7576e006351d73bdcdee2cc
mjkou <mjkou@thoughtworks.com>
2024-10-24 18:13:38 +0800

    add viewmodel

--- app/src/main/java/com/kmj/basestatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/kmj/basestatecodelab/WellnessScreen.kt
@@ -5,19 +5,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kmj.basestatecodelab.ui.theme.BaseStateCodelabTheme
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
     Column(modifier) {
         StatefulCounter()
-
-        val list = remember { getWellnessTasks().toMutableStateList() }
-        WellnessTasksList(list, { task -> list.remove(task) })
+        WellnessTasksList(wellnessViewModel.tasks, { task -> wellnessViewModel.remove(task) })
     }
 }
 
--- app/src/main/java/com/kmj/basestatecodelab/WellnessTasksList.kt
+++ app/src/main/java/com/kmj/basestatecodelab/WellnessTasksList.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
-fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
 
 @Composable
 fun WellnessTasksList(
